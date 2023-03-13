bf7b3d6c21398c536049a13fe78b5b39620c7b75
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-13 16:06:26 +0000

    initial

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,8 +1,23 @@
+import Styles from "./Splash.module.css"
 
 const Splash = () => {
 
     return (
-        <div className=""></div>
+        <>
+            <div className={Styles.main}>
+                <h1>The only bot you'll need to make studying easy.</h1>
+                <p>
+                    A brand new feature rich study <span className={Styles.highlighted}>focused</span> bot, built 
+                    for <span className={Styles.highlighted}>optimizing</span> work output and 
+                    <span className={Styles.highlighted}> maximising</span> rewards to the user.
+                </p>
+                <div className="button-wrapper">
+                    <button>Invite To Server</button>
+                </div>
+            </div>
+
+        </>
     )
 }
 
+export default Splash
