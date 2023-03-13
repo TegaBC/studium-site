a361462f715f86b74df4661cc48f403d7f252c2c
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-13 16:55:36 +0000

    added showcase component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import Navbar from "./components/Navbar"
 import Splash from "./components/Splash"
+import Showcase from "./components/CommandShowcase"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Splash/>
+      <Showcase/>
     </>
   )
 }
