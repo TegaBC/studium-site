3630cdc2e48c1482d8b8fb532b3606a092eeb800
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-15 12:10:06 +0000

    dynamic nav component

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ function App() {
 
   return (
     <>
-      <Navbar/>
-        <Splash/>
-        <Showcase heading="Our commands" paragraph="Keep in mind, this bot is currently in an alpha 
-          testing stage, expect new commands and updates to be added weekly.If you want to be kept in the loop,
-          come back here often to see our latest changes." features={["/points", "/pomodoro", "/check-session", "/uptime"]} />
-          <Leaderboard/>
-        <Showcase heading="Upcoming features" paragraph="Upcoming features that will slowly get added to release versions of Studium." 
-          features={["Reminders", "Time tracking", "Group study sessions"]} headingPadding={false}/>       
-        <Footer/>
+      <Navbar links={[{linkName: "Github", link: "https://github.com/TegaBC/studium-study-bot"}]}/>
+      <Splash/>
+      <Showcase heading="Our commands" paragraph="Keep in mind, this bot is currently in an alpha 
+        testing stage, expect new commands and updates to be added weekly.If you want to be kept in the loop,
+        come back here often to see our latest changes." features={["/points", "/pomodoro", "/check-session", "/uptime"]} />
+        <Leaderboard/>
+      <Showcase heading="Upcoming features" paragraph="Upcoming features that will slowly get added to release versions of Studium." 
+        features={["Reminders", "Time tracking", "Group study sessions"]} headingPadding={false}/>       
+      <Footer/>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,11 @@ import { useState } from "react"
 import NavStyles from "./Navbar.module.css"
 import cx from "classnames"
 
-const nav = () => {
+function renderLink({ link, linkName }) {
+    return <a target="_blank" href={link}>{linkName}</a>
+}
+
+const nav = ({ links }) => {
     const [toggle, setToggle] = useState(false)
 
     return (
@@ -11,7 +15,7 @@ const nav = () => {
                 Studium
             </div>
             <div className={toggle ? cx(NavStyles["nav-links"], NavStyles.toggled) : NavStyles["nav-links"]}>
-                <a href="https://github.com/TegaBC/studium-study-bot">Github</a>
+                {links.map(info => renderLink(info))}
             </div>
             <svg onClick={() => setToggle(!toggle)} className={toggle ? NavStyles.rotated : ""} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
